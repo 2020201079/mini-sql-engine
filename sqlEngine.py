2a044576533e8ae5b8b45a1b51e226ebf3166cee
nambigari <varunnambigari.iit@gmail.com>
2021-01-13 12:30:02 +0530

    Started with query parser class
    using sqlparse library figure out parse

--- sqlEngine.py
+++ sqlEngine.py
@@ -1,10 +1,36 @@
 import os
 import sys
+import sqlparse
+from sqlparse.tokens import Keyword, DML
+
+def printError(error):
+    print(error)
+    exit()
 
 class Table:
     def __init__(self,name):
         self.name = name
         self.attributes = []
+
+class parsedQuery:
+    tables = []
+    colums = []
+    def __init__(self,query):
+        tokenList = sqlparse.parse(query)[0].tokens
+        if(tokenList[0].value.upper() != "SELECT" ):
+            printError("sql query should start with select DML")
+        if not self.fromExistsInToken(tokenList):
+            printError("there is no from keyword in the query")
+        
+    
+    def fromExistsInToken(self,tokenList):
+        for token in tokenList:
+            if(token.ttype is sqlparse.tokens.Keyword and token.value.upper() == "FROM"):
+                return True
+        return False
+        
+
+
 
 def parseMetadataFile(path):
     tables = []
@@ -40,6 +66,26 @@ def parseMetadataFile(path):
 
 def main():
     tables = parseMetadataFile("files/metadata.txt")
+    #sqlQuery = input()
+    sqlQuery = "select max(a) from table1 order by a ASC"
+    parsed = sqlparse.parse(sqlQuery)
+    print(parsed[0].tokens) #0 for first statement. Our condition always has one statement
+    tokenList = parsed[0].tokens
+    for token in tokenList:
+        print(token.ttype," ",token.value)
+    print(sqlparse.sql.Statement(parsed[0]).get_type()) # outputs SELECT
+    print(sqlparse.sql.Identifier(parsed[0]).get_array_indices())
+    
+
+    print(sqlparse.sql.IdentifierList(parsed[0]).get_identifiers())
+    ids = sqlparse.sql.IdentifierList(parsed[0]).get_identifiers()
+    for i in ids:
+        print(i)
+    
+    pq = parsedQuery(sqlQuery)
+
+
+
     
 if __name__ == "__main__":
     main()
